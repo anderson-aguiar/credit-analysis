cf2491f0a320af3c991c7b2fc885452bb622e525
Anderson Martins de Aguiar <anderson.aguiar@agi.com.br>
2026-03-14 17:06:05 -0300

    Criado producer

---
+++ ms-credit/src/main/java/com/anderson/ms_credit/kafka/CreditRequestProducer.java
@@ -0,0 +1,24 @@
+package com.anderson.ms_credit.kafka;
+
+import com.anderson.ms_credit.model.CreditRequestEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class CreditRequestProducer {
+    private static final Logger log = LoggerFactory.getLogger(CreditRequestProducer.class);
+    private final StreamBridge streamBridge;
+    private static final String PRODUCER_TOPIC = "creditRequested-out-0";
+
+    public CreditRequestProducer(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
+    public void sendCreditRequested(CreditRequestEvent event) {
+        log.info("Publishing credit request event: {}", event.requestId());
+        streamBridge.send(PRODUCER_TOPIC, event);
+    }
+}
